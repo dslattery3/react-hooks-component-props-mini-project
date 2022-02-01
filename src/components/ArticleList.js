498e0e6cf5ca4b3e7dc6d483a4eefce9ae276757
Dalton <slattery.da@gmail.com>
2022-01-31 18:29:41 -0600

    commit tree 62d579d4e77959d1a1bff22845bca00eb7b4a2a7

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList({posts}){
+    const articlePost = posts.map((post) => (
+        <Article
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        minutes={post.preview}
+        />
+    ))
+    return(
+        <main>
+            {articlePost}
+        </main>
+    )
+}
+
+export default ArticleList
